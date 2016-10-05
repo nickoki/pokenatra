0ebffc2828777bfa6b2553ace4bd0b78ca4e411d
Nick Oki <nick.oki@live.com>
2016-10-05 11:05:10 -0400

    Init views, routes, layout.erb

--- app.rb
+++ app.rb
@@ -2,10 +2,15 @@ require "active_record"
 require "sinatra"
 require "sinatra/reloader"
 
-
 # Load the file to connect to the DB
 require_relative "db/connection"
 
 # Load models
-require_relative "models/artist"
-require_relative "models/song"
+require_relative "models/pokemon"
+
+
+
+# Routes
+get "/" do
+	erb :"pokemon/index"
+end
