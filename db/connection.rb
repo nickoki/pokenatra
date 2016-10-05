36818ebae6edf40314192398707aec6fbe3aec5c
Nick Oki <nick.oki@live.com>
2016-10-05 10:59:51 -0400

    Add seed data to pokenatra_db, connection.rb fix

--- db/connection.rb
+++ db/connection.rb
@@ -3,6 +3,11 @@ ActiveRecord::Base.establish_connection(
   :database => "pokenatra_db"
 )
 
+# Override Active Record pluralization for "pokemon" to "pokemons"
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.irregular 'pokemon', 'pokemon'
+end
+
 if defined? Sinatra
 	after do
 	  ActiveRecord::Base.connection.close
--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS pokemon;
 CREATE TABLE pokemon (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR UNIQUE,
+	poke_type VARCHAR,
 	cp INT,
 	img_url TEXT
 );
